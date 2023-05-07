73a2454c9b9dda268346a593eadf23c9d969714e
Sabbir <stat1996.bd.iu@gmail.com>
2023-05-07 19:37:16 +0600

    dictionaryadd

---
+++ dd.py
@@ -0,0 +1,22 @@
+'''
+words = {
+    "Aam" : "mango",
+    "kola" : "Banana",
+    "Angur" : "Graps"
+}
+print("Available options: ")
+print(words.keys())
+
+word = input("Enter your bangla word: ")
+
+print(words[word])
+'''
+
+
+
+s = set()
+for i in range (8):
+    num = int(input("enter your number: "))
+    s.add(num)
+print(s)
+
